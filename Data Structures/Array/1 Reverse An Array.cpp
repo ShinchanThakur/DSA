ae47db6835e9d7c14f29deb3a50a2a5bb4228883
ShinchanThakur <rohanthakurarmy@gmail.com>
2022-01-28 07:51:15 +0530

    Reverse an array

---
+++ Data Structures/Array/1 Reverse An Array.cpp
@@ -0,0 +1,19 @@
+//REVERSE AN ARRAY
+
+//ITERATIVE
+void reverseArrayIterative(int arr[], int size){
+    int start = 0, end = size-1;
+    while(start<end)
+        swap(arr[start++], arr[end--]);
+}
+
+//RECURSIVE
+void reverseArrayRecursive(int arr[], int start, int end){
+    if(start>=end)
+        return;
+    swap(arr[start], arr[end]);
+    reverseArrayRecursive(arr, start+1, end-1);
+}
+
+// TIME COMPLEXITY - O(n)
+// SPACE COMPLEXITY - O(1)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# DSA
+> This repository contains important DSA questions with different approaches and their algorithmic analysis.
+> The question list is present at [this](https://drive.google.com/file/d/1FMdN_OCfOI0iAeDlqswCiC2DZzD4nPsb/view) excel sheet.
