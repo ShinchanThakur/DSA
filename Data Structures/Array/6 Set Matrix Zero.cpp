85d10c729ef36b6ee9894d2c1f718d92d21ce888
ShinchanThakur <rohanthakurarmy@gmail.com>
2022-02-08 19:43:44 +0530

    Set Matrix Zero

---
+++ Data Structures/Array/6 Set Matrix Zero.cpp
@@ -0,0 +1,27 @@
+//SET MATRIX ZERO
+
+void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int columns = matrix[0].size();
+        int row0 = 1;
+
+        for(int i=0,j=0;j<columns;j++)
+            if(matrix[i][j]==0)
+                row0 = 0;
+        for(int i=1;i<rows;i++)
+            for(int j=0;j<columns;j++)
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+        for(int i=1;i<rows;i++)
+            for(int j=columns-1;j>=0;j--)
+                if(matrix[i][0]==0 or matrix[0][j]==0)
+                    matrix[i][j] = 0;
+        if(row0==0)
+            for(int i=0,j=0;j<columns;j++)
+                matrix[i][j] = 0;
+    }
+
+//Time Complexity: O(n*m)
+//Space Complexity: O(1)
