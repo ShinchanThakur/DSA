0a0defbdfcf84468e7aabc193b7979f3f5497f2f
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-08-30 07:24:52 +0530

    Improved Middle of LL code

--- Data Structures/Linked List/02 Middle of LL.cpp
+++ Data Structures/Linked List/02 Middle of LL.cpp
@@ -1,15 +1,58 @@
-//FIND MIDDLE OF A LINKED LIST
+// FIND MIDDLE OF A LINKED LIST
 
-ListNode* middleNodeOfLL(ListNode* head) {
-    ListNode *slow = head, *fast = head;
-    while(fast and fast->next){
-        slow = slow->next;
-        fast = fast->next->next;
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    int countNodes(ListNode *head)
+    {
+        int nodeCount = 0;
+        while (head)
+        {
+            nodeCount++;
+            head = head->next;
+        }
+        return nodeCount;
     }
-    return slow;
-}
+    ListNode *getMiddleNodeByCounting(ListNode *head)
+    {
+        int nodeCount = countNodes(head);
+        ListNode *middleNode = head;
+        for (int i = 0; i < nodeCount / 2; i++)
+        {
+            middleNode = middleNode->next;
+        }
+        return middleNode;
+    }
+    ListNode *getMiddleNodeUsingHareAndTortoiseMethod(ListNode *head)
+    {
+        ListNode *slow = head, *fast = head, *middleNode;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        middleNode = slow;
+        return middleNode;
+    }
+    ListNode *middleNode(ListNode *head)
+    {
+        return getMiddleNodeUsingHareAndTortoiseMethod(head);
+    }
+};
 
-//Time Complexity: O(n)
-//Space Complexity: O(1)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
-//Problem Link: https://leetcode.com/problems/middle-of-the-linked-list/
+// Problem Link: https://leetcode.com/problems/middle-of-the-linked-list/
