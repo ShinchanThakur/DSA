b828f13ac5f22ccabec5fbd0e77c2e156ab6cf7a
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-08-31 07:44:23 +0530

    Improved Print sums of all subsets

--- Algorithms/Recursion/01 Print sums of all subsets.cpp
+++ Algorithms/Recursion/01 Print sums of all subsets.cpp
@@ -1,24 +1,27 @@
-//PRINT SUMS OF ALL SUBSETS OF AN ARRAY
+// PRINT SUMS OF ALL SUBSETS OF AN ARRAY
 
-void generateSubsets(vector<int> &arr, vector<int> &subsetSums, int n, int ind, int sum){
-    if(ind == n){
-        subsetSums.push_back(sum);
+void subsetSumsRecursive(vector<int> &subsetSum, int currentIndex, vector<int> &arr, int arrSize, int currentSum)
+{
+    if (currentIndex == arrSize)
+    {
+        subsetSum.push_back(currentSum);
         return;
     }
-    generateSubsets(arr, subsetSums, n, ind + 1, sum);      //not taking element at index ind
-    generateSubsets(arr, subsetSums, n, ind + 1, sum + arr[ind]);       //taking it
+    // take current element
+    subsetSumsRecursive(subsetSum, currentIndex + 1, arr, arrSize, currentSum + arr[currentIndex]);
+    // do not take current element
+    subsetSumsRecursive(subsetSum, currentIndex + 1, arr, arrSize, currentSum);
 }
 
-vector<int> subsetSums(vector<int> arr, int n)
+vector<int> subsetSums(vector<int> arr, int arrSize)
 {
-    vector<int> subsetSums;
-    int ind = 0, sum = 0;
-    generateSubsets(arr, subsetSums, n, ind, sum);
-    sort(subsetSums.begin(), subsetSums.end());
-    return subsetSums;
+    vector<int> subsetSum;
+    int currentIndex = 0, currentSum = 0;
+    subsetSumsRecursive(subsetSum, currentIndex, arr, arrSize, currentSum);
+    return subsetSum;
 }
 
-//Time complexity: O(2^n log(2^n))
-//Space complexity: O(2^n)
+// Time complexity: O(2^n log(2^n))
+// Space complexity: O(2^n)
 
-//Problem link: https://practice.geeksforgeeks.org/problems/subset-sums2234/1#
+// Problem link: https://practice.geeksforgeeks.org/problems/subset-sums2234/1#
